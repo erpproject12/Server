d5f17869570b7354a0220a134b732e6a9764b3c0
nihad313 <mohammedniddu456@gmail.com>
2023-07-20 13:34:23 +0530

    server repo

---
+++ routers/product.js
@@ -0,0 +1,32 @@
+const express = require("express")
+const router = express.Router()
+const Product = require('../model/product')
+const { InsertProduct, ViewProduct, DeleteProduct, UpdateProduct } = require('../controller/product')
+const {InsertParty,ViewParty,UpdateParty,DeleteParty} = require('../controller/party')
+// router.get("/get",(req,res)=>{
+//     res.json({message:"hello"})
+    
+// })
+
+router.post('/insert', InsertProduct )
+
+router.get('/view', ViewProduct )
+
+router.get('/view/:id', ViewProduct )
+
+router.delete('/delete/:id', DeleteProduct )
+
+router.put('/update/:id', UpdateProduct )
+
+// //party
+router.post('/party_insert',InsertParty)
+
+router.get('/view_party',ViewParty)
+
+router.get('/view_party/:id',ViewParty)
+
+router.put('/party_update/:id',UpdateParty)
+
+router.delete('/party_delete/:id',DeleteParty)
+
+module.exports = router
