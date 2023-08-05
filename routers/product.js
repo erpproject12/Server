19931baeb292dc70a125f51352ad9184313de8ce
Likhithakanavu <likhithakanavu@gmail.com>
2023-08-05 09:22:06 +0530

    serverside

--- routers/product.js
+++ routers/product.js
@@ -4,6 +4,7 @@ const Product = require('../model/product')
 const { InsertProduct, ViewProduct, DeleteProduct, UpdateProduct } = require('../controller/product')
 const {InsertParty,ViewParty,UpdateParty,DeleteParty} = require('../controller/party')
 const {InsertPurchase} = require('../controller/purchase')
+const {InsertSales,ViewSales,DeleteSales,UpdateSales} = require('../controller/sales')
 // router.get("/get",(req,res)=>{
 //     res.json({message:"hello"})
     
@@ -32,6 +33,14 @@ router.delete('/party_delete/:id',DeleteParty)
 
 //purchase
 router.post('/purchase_insert',InsertPurchase)
+
+
+//sales
+router.post('/sales_insert',InsertSales)
+router.get('/view_sales',ViewSales)
+router.get('/view_sales/:id',ViewSales)
+router.delete('/delete_sales/:id',DeleteSales)
+router.put('/update_sales/:id',UpdateSales)
 
 
 module.exports = router
